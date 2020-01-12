f2fc2155b6312987ff76eec4546e0b478371bb3f
Nosakhare Belvi <nosakharebelvi@gmail.com>
2020-01-12 08:53:23 +0100

    adding gtb airtime , credit andd debit to filter params

--- datapoint/src/main/java/com/intelia/datapoint/models/FilterParams.kt
+++ datapoint/src/main/java/com/intelia/datapoint/models/FilterParams.kt
@@ -4,6 +4,9 @@ object FilterParams {
     val query = mutableListOf(
 
         DataPointCategory("Airtel_Recharge", mutableListOf("^.*airtelerc.*\$"), mutableListOf("^.*txn.*\$"),DataPointType.SMS),
+        DataPointCategory("Gtb_Airtime", mutableListOf("^.*gtbank.*\$"), mutableListOf("^.*airtel.*\$"),DataPointType.SMS),
+        DataPointCategory("Gtb_Dr", mutableListOf("^.*gtbank.*\$"), mutableListOf("^.*transfer to.*\$"),DataPointType.SMS),
+        DataPointCategory("Gtb_Cr", mutableListOf("^.*gtbank.*\$"), mutableListOf("^.*amt:.*\$"),DataPointType.SMS),
         DataPointCategory("Sterling_Dr", mutableListOf("^.*sterling.*\$"), mutableListOf("^.*debit.*\$"),DataPointType.SMS),
         DataPointCategory("Sterling_Cr", mutableListOf("^.*sterling.*\$"), mutableListOf("^.*credit.*\$"),DataPointType.SMS),
         DataPointCategory("Firstbank_Cr", mutableListOf("^.*firstbank.*\$"), mutableListOf("^.*credited.*\$"),DataPointType.SMS),
